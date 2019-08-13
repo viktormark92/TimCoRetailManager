1ea5339ff045c56fb8307b5d1fd478e3822ad99b
Viktor Mark Toth <viktormark92@gmail.com>
2019-08-13 15:19:22 +0200

    Wire up MVVM in our WPF app

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //On startup I want to launch our shellViewModel as our base view.
+            //it is kind like App.xaml -> StartupUri="ShellView.xaml"
+            //Notice we are not starting the view, we are starting the viewModel, because viewModel will launch the view based upon caliber micro.
+
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
